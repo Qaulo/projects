d25bb40b76331a7bd9e6807401330e3f96532df8
Paul Strunk <ppalust@gmail.com>
2026-02-12 17:00:58 +0100

    added Header with title

--- React/firstReact/src/components/landingpage/landing.tsx
+++ React/firstReact/src/components/landingpage/landing.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Sidebar from "../sidebar/sidebar";
-import { IonHeader, IonContent, IonPage } from "@ionic/react";
+import { IonHeader, IonContent, IonPage, IonTitle } from "@ionic/react";
 import "./landing.css";
 
 function Landing() {
   return (
     <>
       <IonPage>
-        <IonHeader></IonHeader>
         <IonContent>
-          <div className="content">
+          <div className="layout">
             <div className="left">
               <Sidebar></Sidebar>
             </div>
             <div className="right">
+              <IonHeader>
+                <IonTitle>Super Cooler Title</IonTitle>
+              </IonHeader>
               <p>Super Cooler Content</p>
             </div>
           </div>
