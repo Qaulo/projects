e4d3feb263230c674d698ed9365e0d45f41c0e16
Paul Strunk <ppalust@gmail.com>
2026-02-12 16:51:38 +0100

    added grid for content

--- React/firstReact/src/components/landingpage/landing.tsx
+++ React/firstReact/src/components/landingpage/landing.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Sidebar from "../sidebar/sidebar";
-import { IonHeader, IonContent } from "@ionic/react";
+import { IonHeader, IonContent, IonPage } from "@ionic/react";
+import "./landing.css";
 
 function Landing() {
   return (
     <>
-      <Sidebar></Sidebar>
-      <IonHeader></IonHeader>
-      <IonContent>Super Cooler content</IonContent>
+      <IonPage>
+        <IonHeader></IonHeader>
+        <IonContent>
+          <div className="content">
+            <div className="left">
+              <Sidebar></Sidebar>
+            </div>
+            <div className="right">
+              <p>Super Cooler Content</p>
+            </div>
+          </div>
+        </IonContent>
+      </IonPage>
     </>
   );
 }
