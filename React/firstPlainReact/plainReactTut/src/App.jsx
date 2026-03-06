4c76993d17fd83313052a184057b1d9e0031fc7e
Paul Strunk <ppalust@gmail.com>
2026-03-06 23:04:27 +0100

    startet with tiktaktoe project from official React.dev website to learn React without a fancy framework

---
+++ React/firstPlainReact/plainReactTut/src/App.jsx
@@ -0,0 +1,10 @@
+{
+  /*React Tutorial from https://react.dev/learn/tutorial-tic-tac-toe */
+}
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+{
+  /*Wo war ich: Inspection the starter code */
+}
