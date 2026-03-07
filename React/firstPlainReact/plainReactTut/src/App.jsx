7d7b290fb00607bb7cb477e178952cab0165fd68
Paul Strunk <ppalust@gmail.com>
2026-03-07 20:22:47 +0100

    weiter an tiktaktoe

--- React/firstPlainReact/plainReactTut/src/App.jsx
+++ React/firstPlainReact/plainReactTut/src/App.jsx
@@ -1,10 +1,45 @@
 {
   /*React Tutorial from https://react.dev/learn/tutorial-tic-tac-toe */
 }
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+import Square from "./Square.jsx";
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!setXIsNext);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
 
 {
-  /*Wo war ich: Inspection the starter code */
+  /*Wo war ich: Taking turns Das Taking turn funktioniert noch nicht ganz es bleibt immer ein O nachdem X */
 }
